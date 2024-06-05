1986c91dfbacfeea2cf77b015035205bce7a3902
seungdangdang <yseungun.21@gmail.com>
2024-06-05 14:52:48 +0900

    feat: 유형 별 해당 멤버의 브랜딩 페이지를 리다이렉트하는 서블릿 기능 구현

---
+++ src/main/java/com/nuguna/freview/servlet/member/MemberBrandingServlet.java
@@ -0,0 +1,33 @@
+package com.nuguna.freview.servlet.member;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/MemberBrandingServlet")
+public class MemberBrandingServlet extends HttpServlet {
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    String gubun = req.getParameter("gubun");
+    String mid = req.getParameter("mid");
+
+    switch (gubun) {
+      case "B":
+        System.out.println("gubun = B");
+        resp.sendRedirect(req.getContextPath() + "/boss-branding-page.jsp?mid=" + mid);
+        break;
+      case "C":
+        System.out.println("gubun = C");
+        resp.sendRedirect(req.getContextPath() + "/customer-branding-page.jsp?mid=" + mid);
+        break;
+      default:
+        resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid user type");
+        break;
+    }
+  }
+}
