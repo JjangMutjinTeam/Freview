b2f330224854b9fc70c17bae2bab3ec221d9e475
seungdangdang <146111105+seungdangdang@users.noreply.github.com>
2024-06-05 15:17:49 +0900

    [feat-admin] 어드민 유저관리 페이지 기능 구현 (#1)
    * feat: 모든 멤버를 조회하는 dao 구현
    
    * feat: 모든 멤버를 조회하여 admin-mg-users-y.jsp 로 전달하는 기능 구현
    
    * feat: servlet 을 통해 전달 받은 모든 멤버 목록을 화면에서 확인할 수 있는 기능 구현
    
    * feat: 멤버 탈퇴를 위해 특정 멤버를 삭제하는 dao / 관리자 비밀번호를 가져오는 dao 기능 구현
    
    * feat: 멤버 탈퇴를 컨트롤하는 서블릿 기능 구현
    
    * feat: 유저 목록 화면에서 멤버 탈퇴 버튼과 탈퇴 버튼에 대한 로직 구현
    
    * chore: 화면 출력 내용을 현재 템플릿에 맞게 수정
    
    * chore: adminDAO의 쿼리문 이름을 용도에 적합하게 수정
    
    * refactor: 관리자 비밀번호 검증 코드의 가독성을 위해 메서드로 분리
    
    * feat: 유형 별 해당 멤버의 브랜딩 페이지를 리다이렉트하는 서블릿 기능 구현
    
    * feat: 유저 목록 화면에서 아이디 클릭 시 구분과 아이디를 MemberBrandingServlet 으로 전달하는 기능 구현

--- src/main/java/com/nuguna/freview/servlet/admin/AdminServlet.java
+++ src/main/java/com/nuguna/freview/servlet/admin/AdminServlet.java
@@ -1,25 +1,53 @@
 package com.nuguna.freview.servlet.admin;
 
+import com.nuguna.freview.dao.admin.AdminDAO;
+import com.nuguna.freview.entity.member.Member;
 import java.io.IOException;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet
+@WebServlet("/AdminPage")
 public class AdminServlet extends HttpServlet {
 
-  @Override
-  public void init() throws ServletException {
-  }
+  AdminDAO adminDAO = new AdminDAO();
 
   @Override
-  protected void service(HttpServletRequest req, HttpServletResponse resp)
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
+    req.setCharacterEncoding("UTF-8");
+    resp.setContentType("text/html;charset=UTF-8");
+    List<Member> memberAllList = adminDAO.selectAllMember();
+    req.setAttribute("memberAllList", memberAllList);
+    RequestDispatcher rd = req.getRequestDispatcher("admin-mg-users-y.jsp");
+    rd.forward(req, resp);
   }
 
   @Override
-  public void destroy() {
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    String userId = req.getParameter("userId");
+    String inputPassword = req.getParameter("password");
+    String adminPassword = adminDAO.selectAdminPW();
+
+    if (isPasswordMatch(inputPassword, adminPassword)) {
+      boolean isDeleted = adminDAO.deleteMember(userId);
+
+      if (isDeleted) {
+        resp.setStatus(HttpServletResponse.SC_OK);
+      } else {
+        resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+      }
+    } else {
+      resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+  }
+
+  private boolean isPasswordMatch(String inputPassword, String adminPassword) {
+    return inputPassword.equals(adminPassword);
   }
 }
