d11d65410c4be756f16f81cd55baf8090180d1e5
seungdangdang <yseungun.21@gmail.com>
2024-06-06 22:45:09 +0900

    feat: 커서 기반의 페이지네이션을 통해 글 post 를 가져오는 dao 기능 구현

--- src/main/java/com/nuguna/freview/dao/post/PostDAO.java
+++ src/main/java/com/nuguna/freview/dao/post/PostDAO.java
@@ -5,17 +5,64 @@ import static com.nuguna.freview.config.DbConfig.DB_URL;
 import static com.nuguna.freview.config.DbConfig.DB_USER;
 import static com.nuguna.freview.config.DbConfig.DRIVER_NAME;
 
+import com.nuguna.freview.entity.post.Post;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class PostDAO {
 
+  private final int LIMIT = 10;
+
+  private final String SELECT_POST_BY_PAGING = " SELECT post_seq, title, view_count, created_at "
+      + "FROM POST "
+      + "WHERE gubun = ? and post_seq < ? "
+      + "ORDER BY post_seq DESC "
+      + "limit ?";
+
   private final String COUNT_POST = "SELECT COUNT(1) FROM POST WHERE gubun = ?";
+
+  public List<Post> selectPostByPaging(String gubun, int previousPostSeq) {
+    Connection conn = null;
+    PreparedStatement pstmt = null;
+    ResultSet rs = null;
+
+    System.out.println("previousPostSeq: " + previousPostSeq);
+
+    List<Post> list = new ArrayList<>();
+
+    try {
+      conn = getConnection();
+      pstmt = conn.prepareStatement(SELECT_POST_BY_PAGING);
+      pstmt.setString(1, gubun);
+      pstmt.setInt(2, previousPostSeq);
+      pstmt.setInt(3, LIMIT);
+      rs = pstmt.executeQuery();
+
+      while(rs.next()) {
+        Post post = new Post();
+        post.setPostSeq(rs.getInt("post_seq"));
+        post.setTitle(rs.getString("title"));
+        post.setViewCount(rs.getInt("view_count"));
+        post.setCreatedAt(rs.getTimestamp("created_at"));
+
+        list.add(post);
+      }
+
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    } finally {
+      closeResource(pstmt, conn, rs);
+    }
+
+    return list;
+  }
 
   public int countTotalPost(String gubun) {
     Connection conn = null;
