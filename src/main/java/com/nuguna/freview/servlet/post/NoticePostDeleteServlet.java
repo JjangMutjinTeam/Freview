6e79909c1644bcfe1cf0fa68344ed82c73c74e15
seungdangdang <yseungun.21@gmail.com>
2024-06-07 19:23:08 +0900

    feat: 특정 공지글 delete 를 컨트롤하는 서블릿 구현

--- src/main/java/com/nuguna/freview/servlet/post/NoticePostDeleteServlet.java
+++ src/main/java/com/nuguna/freview/servlet/post/NoticePostDeleteServlet.java
@@ -1,18 +1,15 @@
 package com.nuguna.freview.servlet.post;
 
 import com.nuguna.freview.dao.post.PostDAO;
-import com.nuguna.freview.entity.post.Post;
 import java.io.IOException;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/noticeBoard/detail/update")
-public class NoticePostUpdateServlet extends HttpServlet {
+@WebServlet("/noticeBoard/detail/delete")
+public class NoticePostDeleteServlet extends HttpServlet {
 
   PostDAO postDAO = new PostDAO();
 
@@ -23,19 +20,10 @@ public class NoticePostUpdateServlet extends HttpServlet {
     resp.setCharacterEncoding("UTF-8");
 
     int postSeq = Integer.parseInt(req.getParameter("postSeq"));
-    String title = req.getParameter("title");
-    String content = req.getParameter("content");
-    Timestamp now = Timestamp.valueOf(LocalDateTime.now());
 
-    Post post = new Post();
-    post.setPostSeq(postSeq);
-    post.setTitle(title);
-    post.setContent(content);
-    post.setUpdatedAt(now);
+    boolean deletePost = postDAO.deletePost(postSeq);
 
-    boolean updatePost = postDAO.updatePost(post);
-
-    if (updatePost) {
+    if (deletePost) {
       resp.setStatus(HttpServletResponse.SC_OK);
     } else {
       resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
