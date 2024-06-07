4feef722c805d0790873d76b68ef0604f2ec70d4
wonjun <wonjon0930@daum.net>
2024-06-07 11:50:24 +0900

    feat: 체험단 리뷰 URL 변경 API 구현 완료

--- src/main/java/com/nuguna/freview/dao/member/MemberDAO.java
+++ src/main/java/com/nuguna/freview/dao/member/MemberDAO.java
@@ -38,4 +38,31 @@ public class MemberDAO {
     return MemberGubun.from(gubun);
   }
 
+  public boolean doesMemberOwnReview(int memberSeq, int reviewSeq) {
+    Connection conn = null;
+    PreparedStatement pstmt = null;
+    ResultSet rs = null;
+
+    String sql = "SELECT 1 FROM REVIEW "
+        + "WHERE review.cust_seq = ? "
+        + "AND review.review_seq = ?";
+
+    boolean isMatched = false;
+    try {
+      conn = getConnection();
+      pstmt = conn.prepareStatement(sql);
+      pstmt.setInt(1, memberSeq);
+      pstmt.setInt(2, reviewSeq);
+      rs = pstmt.executeQuery();
+      if (rs.next()) {
+        isMatched = true;
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException("SQLException : 소개 변경 도중 예외 발생", e);
+    } finally {
+      closeResource(pstmt, conn, rs);
+    }
+    return isMatched;
+  }
+
 }
--- src/main/java/com/nuguna/freview/servlet/member/api/cust/mybrand/ReviewUrlSubmitServlet.java
+++ src/main/java/com/nuguna/freview/servlet/member/api/cust/mybrand/ReviewUrlSubmitServlet.java
@@ -1,21 +1,15 @@
 package com.nuguna.freview.servlet.member.api.cust.mybrand;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
-import com.nuguna.freview.dao.member.CustTagDAO;
+import com.nuguna.freview.dao.member.CustReviewDAO;
 import com.nuguna.freview.dao.member.MemberDAO;
 import com.nuguna.freview.dto.common.ResponseMessage;
-import com.nuguna.freview.entity.member.MemberGubun;
-import com.nuguna.freview.exception.IllegalTagException;
 import com.nuguna.freview.util.EncodingUtil;
 import com.nuguna.freview.util.JsonRequestUtil;
 import com.nuguna.freview.util.JsonResponseUtil;
 import java.io.IOException;
-import java.util.List;
-import java.util.stream.Collectors;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -24,19 +18,19 @@ import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@WebServlet("/api/cust/my-brand/tag")
-public class TagUpdateServlet extends HttpServlet {
+@WebServlet("/api/cust/my-brand/review")
+public class ReviewUrlSubmitServlet extends HttpServlet {
 
   private Gson gson;
   private MemberDAO memberDAO;
-  private CustTagDAO custTagDAO;
+  private CustReviewDAO custReviewDAO;
 
   @Override
   public void init() throws ServletException {
-    log.info("TagUpdateServlet 초기화");
+    log.info("ReviewUrlSubmitServlet 초기화");
     gson = new Gson();
     memberDAO = new MemberDAO();
-    custTagDAO = new CustTagDAO();
+    custReviewDAO = new CustReviewDAO();
   }
 
   @Override
@@ -45,7 +39,7 @@ public class TagUpdateServlet extends HttpServlet {
 
     EncodingUtil.setEncodingToUTF8AndJson(request, response);
 
-    log.info("TagUpdateServlet.doPost");
+    log.info("ReviewUrlSubmitServlet.doPost");
 
     try {
       JsonObject jsonObject = JsonRequestUtil.parseJson(request.getReader(), gson);
@@ -53,36 +47,25 @@ public class TagUpdateServlet extends HttpServlet {
       // TODO : 추후 Input Data가 NULL 인 경우 또한 처리해주어야 함.
       // TODO : 서블릿 필터에서 memberSeq의 유효성을 체크해준다고 가정
       int memberSeq = jsonObject.get("member_seq").getAsInt();
-      JsonArray tags = jsonObject.get("to_tags").getAsJsonArray();
+      int reviewSeq = jsonObject.get("review_seq").getAsInt();
+      String toReviewUrl = jsonObject.get("to_review_url").getAsString();
 
-      List<String> tagNames = tags.asList().stream()
-          .map(JsonElement::getAsString)
-          .collect(Collectors.toList());
-
-      MemberGubun memberGubun = memberDAO.selectMemberGubun(memberSeq);
-
-      if (!memberGubun.isCust()) {
-        JsonResponseUtil.sendBackJsonWithStatus(HttpServletResponse.SC_BAD_REQUEST,
-            new ResponseMessage<>("해당 유저의 구분은 체험단이 아닙니다.", tagNames), response, gson);
-      } else {
-        custTagDAO.updateTags(memberSeq, tagNames);
-
+      if (memberDAO.doesMemberOwnReview(memberSeq, reviewSeq)) { // 리뷰 주인인지 검증
+        custReviewDAO.updateReviewUrl(reviewSeq, toReviewUrl);
         JsonResponseUtil.sendBackJsonWithStatus(HttpServletResponse.SC_OK,
-            new ResponseMessage<>("성공적으로 수정되었습니다.", tagNames), response, gson);
+            new ResponseMessage<>("성공적으로 수정했습니다.", toReviewUrl), response, gson);
+      } else {
+        JsonResponseUtil.sendBackJsonWithStatus(HttpServletResponse.SC_BAD_REQUEST,
+            new ResponseMessage<>("해당 체험단은 리뷰 주인이 아닙니다.", null), response, gson);
       }
     } catch (JsonParseException e) {
-      log.error("태그 변경 요청에 대한 JSON 파싱 에러가 발생했습니다.", e);
+      log.error("리뷰 URL 변경 요청에 대한 JSON 파싱 에러가 발생했습니다.", e);
       JsonResponseUtil.sendBackJsonWithStatus(HttpServletResponse.SC_BAD_REQUEST,
           new ResponseMessage<>("요청 JSON의 형식에 문제가 있습니다.", null), response, gson);
-    } catch (IllegalTagException e) {
-      log.error("유효하지 않은 체험단용 태그입니다.", e);
-      JsonResponseUtil.sendBackJsonWithStatus(HttpServletResponse.SC_BAD_REQUEST,
-          new ResponseMessage<>("유효하지 않은 체험단용 태그입니다.", null), response, gson);
     } catch (Exception e) {
-      log.error("태그 변경 도중 서버 에러가 발생했습니다.", e);
+      log.error("리뷰 URL 변경 도중 서버 에러가 발생했습니다.", e);
       JsonResponseUtil.sendBackJsonWithStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
-          new ResponseMessage<>("태그 변경 도중 서버 에러가 발생했습니다.", null), response, gson);
+          new ResponseMessage<>("리뷰 URL 변경 도중 서버 에러가 발생했습니다.", null), response, gson);
     }
   }
-
 }
