86f69785efb0a0bd70eafd433705c7868ff43b2a
seungdangdang <yseungun.21@gmail.com>
2024-06-07 09:17:54 +0900

    feat: 공지게시판의 페이지네이션을 컨트롤하는 서블릿 구현

---
+++ src/main/java/com/nuguna/freview/servlet/post/NoticeBoardServlet.java
@@ -0,0 +1,38 @@
+package com.nuguna.freview.servlet.post;
+
+import com.nuguna.freview.dao.post.PostDAO;
+import com.nuguna.freview.entity.post.Post;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/noticeBoard")
+public class NoticeBoardServlet extends HttpServlet {
+  PostDAO postDAO = new PostDAO();
+
+  private final int LIMIT = 10;
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    String postGubun = "GJ";
+    req.setCharacterEncoding("UTF-8");
+    resp.setContentType("text/html;charset=UTF-8");
+
+    int previousPostSeq = Integer.MAX_VALUE;
+    if (req.getParameter("previousPostSeq") != null) {
+      previousPostSeq = Integer.parseInt(req.getParameter("previousPostSeq"));
+    }
+
+    List<Post> postList = postDAO.selectPostByPaging(postGubun, previousPostSeq, LIMIT);
+    req.setAttribute("postList", postList);
+
+    RequestDispatcher rd = req.getRequestDispatcher("/common-notice-board-y.jsp");
+    rd.forward(req, resp);
+  }
+}
