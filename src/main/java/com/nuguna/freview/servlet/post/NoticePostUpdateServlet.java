714ec62b9855e64c13969f90331523acfae6e05b
seungdangdang <yseungun.21@gmail.com>
2024-06-07 18:29:57 +0900

    feat: 특정 공지글을 update 를 컨트롤하는 서블릿 구현

---
+++ src/main/java/com/nuguna/freview/servlet/post/NoticePostUpdateServlet.java
@@ -0,0 +1,45 @@
+package com.nuguna.freview.servlet.post;
+
+import com.nuguna.freview.dao.post.PostDAO;
+import com.nuguna.freview.entity.post.Post;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/noticeBoard/detail/update")
+public class NoticePostUpdateServlet extends HttpServlet {
+
+  PostDAO postDAO = new PostDAO();
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+
+    req.setCharacterEncoding("UTF-8");
+    resp.setCharacterEncoding("UTF-8");
+
+    int postSeq = Integer.parseInt(req.getParameter("postSeq"));
+    String title = req.getParameter("title");
+    String content = req.getParameter("content");
+    Timestamp now = Timestamp.valueOf(LocalDateTime.now());
+
+    Post post = new Post();
+    post.setPostSeq(postSeq);
+    post.setTitle(title);
+    post.setContent(content);
+    post.setUpdatedAt(now);
+
+    boolean updateResult = postDAO.updatePost(post);
+
+    if (updateResult) {
+      resp.setStatus(HttpServletResponse.SC_OK);
+    } else {
+      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    }
+  }
+}
