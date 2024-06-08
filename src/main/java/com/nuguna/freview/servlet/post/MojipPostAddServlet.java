53b6b0551203bd4de550eda90676db6d67b3a247
seungdangdang <yseungun.21@gmail.com>
2024-06-08 18:46:57 +0900

    feat: 새로운 모집글을 등록하는 서블릿 구현

--- src/main/java/com/nuguna/freview/servlet/post/MojipPostAddServlet.java
+++ src/main/java/com/nuguna/freview/servlet/post/MojipPostAddServlet.java
@@ -1,10 +1,11 @@
 package com.nuguna.freview.servlet.post;
 
-import static com.nuguna.freview.entity.post.PostGubun.GJ;
+import static com.nuguna.freview.entity.post.PostGubun.MJ;
 
 import com.nuguna.freview.dao.post.PostDAO;
 import com.nuguna.freview.entity.post.Post;
 import java.io.IOException;
+import java.sql.Date;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import javax.servlet.ServletException;
@@ -13,15 +14,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/noticeBoard/createPost")
-public class NoticePostAddServlet extends HttpServlet {
+@WebServlet("/mojipBoard/createPost")
+public class MojipPostAddServlet extends HttpServlet {
 
   PostDAO postDAO = new PostDAO();
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
-    resp.sendRedirect(req.getContextPath() + "/admin-create-notice-y.jsp");
+    resp.sendRedirect(req.getContextPath() + "/boss-create-mojip-y.jsp");
   }
 
   @Override
@@ -34,13 +35,17 @@ public class NoticePostAddServlet extends HttpServlet {
 
     Post post = new Post();
     post.setTitle(req.getParameter("title"));
+    post.setMemberSeq(Integer.valueOf(req.getParameter("memberSeq")));
+    post.setApplyStartDate(Date.valueOf(req.getParameter("applyStartDate")));
+    post.setApplyEndDate(Date.valueOf(req.getParameter("applyEndDate")));
+    post.setExperienceDate(Date.valueOf(req.getParameter("experienceDate")));
     post.setContent(req.getParameter("content"));
-    post.setGubun(GJ.getCode());
+    post.setGubun(MJ.getCode());
+    post.setThumbnailPhotoUrl(req.getParameter("thumbnailPhotoUrl"));
     post.setCreatedAt(now);
     post.setUpdatedAt(now);
-    post.setMemberSeq(Integer.valueOf(req.getParameter("memberSeq")));
 
-    boolean insertPost = postDAO.insertNoticePost(post);
+    boolean insertPost = postDAO.insertMojipPost(post);
 
     if (insertPost) {
       resp.setStatus(HttpServletResponse.SC_OK);
