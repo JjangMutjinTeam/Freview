127e886a0490071b1701806b9eccf6cfa9d4771d
seungdangdang <yseungun.21@gmail.com>
2024-06-09 15:56:36 +0900

    feat: 접속자의 글 좋아요 여부를 전달하는 servlet 기능 구현

--- src/main/java/com/nuguna/freview/servlet/post/MojipPostDetailServlet.java
+++ src/main/java/com/nuguna/freview/servlet/post/MojipPostDetailServlet.java
@@ -1,6 +1,7 @@
 package com.nuguna.freview.servlet.post;
 
 import com.nuguna.freview.dao.post.MojipPostDAO;
+import com.nuguna.freview.dao.post.PostDAO;
 import com.nuguna.freview.dto.MojipPostDTO;
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -13,13 +14,19 @@ import javax.servlet.http.HttpServletResponse;
 public class MojipPostDetailServlet extends HttpServlet {
 
   MojipPostDAO mojipPostDAO = new MojipPostDAO();
+  PostDAO postDAO = new PostDAO();
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
     int postSeq = Integer.parseInt(req.getParameter("postSeq"));
+    int memberSeq = 11; //TODO: 로그인한 사용자의 memberSeq 필요
+
     MojipPostDTO mojipPost = mojipPostDAO.getMojipPost(postSeq);
+    boolean isLiked = postDAO.isLikedPost(memberSeq, postSeq);
+
     req.setAttribute("mojipPost", mojipPost);
+    req.setAttribute("isLiked", isLiked);
     req.getRequestDispatcher("/common-mojip-post-detail-y.jsp").forward(req, resp);
   }
 }
