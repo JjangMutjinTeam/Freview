43e8622cf4ad52c764a80fbeeed2072787a4fde9
seungdangdang <yseungun.21@gmail.com>
2024-07-01 20:42:13 +0900

    feat: 페이지번호에 따라 글 리스트를 반환하는 서블릿 기능 구현

--- src/main/java/com/nuguna/freview/servlet/post/NoticeBoardServlet.java
+++ src/main/java/com/nuguna/freview/servlet/post/NoticeBoardServlet.java
@@ -1,39 +1,76 @@
 package com.nuguna.freview.servlet.post;
 
+import com.google.gson.Gson;
 import com.nuguna.freview.dao.post.NoticePostDAO;
+import com.nuguna.freview.dao.post.PostDAO;
+import com.nuguna.freview.entity.member.Member;
 import com.nuguna.freview.entity.post.Post;
+import com.nuguna.freview.entity.post.PostGubun;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
-import javax.servlet.RequestDispatcher;
+import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/notice")
 public class NoticeBoardServlet extends HttpServlet {
 
   NoticePostDAO noticePostDAO = new NoticePostDAO();
+  PostDAO postDAO = new PostDAO();
 
   private final int LIMIT = 10;
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
-    String postGubun = "GJ";
     req.setCharacterEncoding("UTF-8");
     resp.setContentType("text/html;charset=UTF-8");
 
-    int previousPostSeq = Integer.MAX_VALUE;
-    if (req.getParameter("previousPostSeq") != null) {
-      previousPostSeq = Integer.parseInt(req.getParameter("previousPostSeq"));
+    HttpSession session = req.getSession();
+    Member loginUser = (Member) session.getAttribute("Member");
+
+    //TODO: 비로그인 시 로그인페이지로 이동하는 메서드 유틸로 작성하기
+    if (loginUser == null) {
+      resp.sendRedirect("common-login.jsp");
+      return;
     }
 
-    List<Post> postList = noticePostDAO.selectNoticePostByCursorPaging(postGubun, previousPostSeq, LIMIT);
-    req.setAttribute("postList", postList);
+    req.setAttribute("loginUser", loginUser);
 
-    RequestDispatcher rd = req.getRequestDispatcher("/common-notice-board-y.jsp");
-    rd.forward(req, resp);
+    req.getRequestDispatcher("/common-notice-board-y.jsp").forward(req, resp);
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    req.setCharacterEncoding("UTF-8");
+    resp.setContentType("application/json;charset=UTF-8");
+
+    int pageNumber = getPageNumber(req);
+    String postGubun = PostGubun.GJ.getCode();
+    List<Post> postList = noticePostDAO.getNoticeByPage(postGubun, pageNumber, LIMIT);
+    int totalPosts = postDAO.getTotalPostsCount(postGubun);
+    int totalPages = (int) Math.ceil((double) totalPosts / LIMIT);
+
+    Map<String, Object> responseMap = new HashMap<>();
+    responseMap.put("data", postList);
+    responseMap.put("totalPages", totalPages);
+
+    Gson gson = new Gson();
+    String jsonResponse = gson.toJson(responseMap);
+    resp.getWriter().write(jsonResponse);
+  }
+
+  private int getPageNumber(HttpServletRequest req) {
+    try {
+      return Integer.parseInt(req.getParameter("page"));
+    } catch (NumberFormatException e) {
+      return 1;
+    }
   }
 }
