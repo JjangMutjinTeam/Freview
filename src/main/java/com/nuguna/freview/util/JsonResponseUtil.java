4f8ee1f9e9293573927fd393af31144e933ff1c4
seungdangdang <yseungun.21@gmail.com>
2024-07-04 16:29:34 +0900

    feat: 공통Util 메서드 추가

--- src/main/java/com/nuguna/freview/util/JsonResponseUtil.java
+++ src/main/java/com/nuguna/freview/util/JsonResponseUtil.java
@@ -3,6 +3,7 @@ package com.nuguna.freview.util;
 import com.google.gson.Gson;
 import com.nuguna.freview.dto.common.ResponseMessage;
 import java.io.IOException;
+import java.util.Map;
 import javax.servlet.http.HttpServletResponse;
 
 public class JsonResponseUtil {
@@ -14,5 +15,11 @@ public class JsonResponseUtil {
     response.setStatus(statusCode);
     String jsonStr = gson.toJson(responseJsonSource);
     response.getWriter().print(jsonStr);
+  }
+
+  public static <T> void sendJsonResponse(Map<String, Object> responseMap, HttpServletResponse response, Gson gson)
+      throws IOException {
+    String jsonResponse = gson.toJson(responseMap);
+    response.getWriter().write(jsonResponse);
   }
 }
