70962b051469bd0ae7c87dbdea289541f2684efc
seungdangdang <yseungun.21@gmail.com>
2024-07-07 14:27:52 +0900

    feat: 멤버관리페이지 검색어가 있을 경우 해당되는 멤버 리스트를 불러오는 dao 구현

--- src/main/java/com/nuguna/freview/dao/admin/AdminDAO.java
+++ src/main/java/com/nuguna/freview/dao/admin/AdminDAO.java
@@ -92,6 +92,55 @@ public class AdminDAO {
     return list;
   }
 
+  public List<Member> getMemberList(int previousMemberSeq, int LIMIT, String searchWord) {
+    String sql = "SELECT * FROM member WHERE gubun != 'A' AND member_seq < ? " +
+        "AND (id LIKE CONCAT('%', ?, '%') OR nickname LIKE CONCAT('%', ?, '%')) " +
+        "ORDER BY member_seq DESC LIMIT ?";
+
+    Connection conn = null;
+    PreparedStatement pstmt = null;
+    ResultSet rs = null;
+
+    List<Member> list = new ArrayList<>();
+
+    try {
+      conn = getConnection();
+      pstmt = conn.prepareStatement(sql);
+      int paramIndex = 1;
+      pstmt.setInt(paramIndex++, previousMemberSeq);
+      pstmt.setString(paramIndex++, searchWord);
+      pstmt.setString(paramIndex++, searchWord);
+      pstmt.setInt(paramIndex, LIMIT);
+
+      rs = pstmt.executeQuery();
+
+      while (rs.next()) {
+        Member member = new Member();
+        member.setMemberSeq(rs.getInt(1));
+        member.setGubun(rs.getString(2));
+        member.setId(rs.getString(3));
+        member.setPw(rs.getString(4));
+        member.setNickname(rs.getString(5));
+        member.setEmail(rs.getString(6));
+        member.setAgeGroup(rs.getString(7));
+        member.setIntroduce(rs.getString(8));
+        member.setBusinessNumber(rs.getString(9));
+        member.setStoreLocation(rs.getString(10));
+        member.setProfilePhotoUrl(rs.getString(11));
+        member.setCreatedAt(rs.getTimestamp(12));
+        member.setUpdatedAt(rs.getTimestamp(13));
+
+        list.add(member);
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    } finally {
+      closeResource(pstmt, conn, rs);
+    }
+
+    return list;
+  }
+
   public boolean deleteMember(String memberId) {
     boolean isDeleted = false;
     Connection conn = null;
