9c9263480915755d5db9b7ff9ee81e2b27ddf11b
seungdangdang <yseungun.21@gmail.com>
2024-07-08 16:37:36 +0900

    fix: 브랜치 충돌 이슈 해결-2

--- src/main/java/com/nuguna/freview/dao/admin/AdminDAO.java
+++ src/main/java/com/nuguna/freview/dao/admin/AdminDAO.java
@@ -18,39 +18,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class AdminDAO {
 
-  private final String DELETE_MEMBER_BY_ID = "DELETE FROM member WHERE id = ?";
-  private final String SELECT_ADMIN_PW = "SELECT pw FROM member WHERE gubun = 'A'";
-  private final String SELECT_STORE_BUSINESS_INFO = "SELECT s.store_name, s.business_number, m.id, m.created_at FROM store_business_info s LEFT JOIN member m ON s.business_number = m.business_number";
-  private final String DELETE_STORE_BY_BUSINESS_NUMBER = "DELETE FROM store_business_info WHERE business_number = ?";
-  private final String INSERT_STORE = "INSERT INTO store_business_info(business_number, store_name) VALUES(?, ?)";
-
-  public boolean getMatchingMember(String memberPw) {
-    //TODO: 암호 암호화 메서드 활용
-//    String encryptedPw =
-    Connection conn = null;
-    PreparedStatement pstmt = null;
-    ResultSet rs = null;
-
-    String sql = "select count(*) from member where gubun = 'A' and pw = ?";
-    try {
-      conn = getConnection();
-      pstmt = conn.prepareStatement(sql);
-      pstmt.setString(1, memberPw);
-      rs = pstmt.executeQuery();
-
-      if (rs.next()) {
-        int count = rs.getInt(1);
-        return count > 0;
-      }
-      return false;
-
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
-    } finally {
-      closeResource(pstmt, conn, rs);
-    }
-  }
-
   public AdminPersonalInfoDTO getAdminPersonalInfo(int memberSeq) {
     String sql = "select member_seq, gubun, id, nickname, email from member where member_seq = ?";
 
@@ -82,8 +49,8 @@ public class AdminDAO {
     }
   }
 
-  public List<Member> getMemberList(int previousMemberSeq, int limit) {
-    String sql = "SELECT * FROM member WHERE gubun != 'A' AND member_seq < ? ORDER BY member_seq DESC LIMIT ?";
+  public List<Member> selectAllMember() {
+    String sql = "SELECT * FROM member WHERE gubun != 'A'";
 
     Connection conn = null;
     PreparedStatement pstmt = null;
@@ -94,57 +61,6 @@ public class AdminDAO {
     try {
       conn = getConnection();
       pstmt = conn.prepareStatement(sql);
-      pstmt.setInt(1, previousMemberSeq);
-      pstmt.setInt(2, limit);
-      rs = pstmt.executeQuery();
-
-      while (rs.next()) {
-        Member member = new Member();
-        member.setMemberSeq(rs.getInt(1));
-        member.setGubun(rs.getString(2));
-        member.setId(rs.getString(3));
-        member.setPw(rs.getString(4));
-        member.setNickname(rs.getString(5));
-        member.setEmail(rs.getString(6));
-        member.setAgeGroup(rs.getString(7));
-        member.setIntroduce(rs.getString(8));
-        member.setBusinessNumber(rs.getString(9));
-        member.setStoreLocation(rs.getString(10));
-        member.setProfilePhotoUrl(rs.getString(11));
-        member.setCreatedAt(rs.getTimestamp(12));
-        member.setUpdatedAt(rs.getTimestamp(13));
-
-        list.add(member);
-      }
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
-    } finally {
-      closeResource(pstmt, conn, rs);
-    }
-
-    return list;
-  }
-
-  public List<Member> getMemberList(int previousMemberSeq, int LIMIT, String searchWord) {
-    String sql = "SELECT * FROM member WHERE gubun != 'A' AND member_seq < ? " +
-        "AND (id LIKE CONCAT('%', ?, '%') OR nickname LIKE CONCAT('%', ?, '%')) " +
-        "ORDER BY member_seq DESC LIMIT ?";
-
-    Connection conn = null;
-    PreparedStatement pstmt = null;
-    ResultSet rs = null;
-
-    List<Member> list = new ArrayList<>();
-
-    try {
-      conn = getConnection();
-      pstmt = conn.prepareStatement(sql);
-      int paramIndex = 1;
-      pstmt.setInt(paramIndex++, previousMemberSeq);
-      pstmt.setString(paramIndex++, searchWord);
-      pstmt.setString(paramIndex++, searchWord);
-      pstmt.setInt(paramIndex, LIMIT);
-
       rs = pstmt.executeQuery();
 
       while (rs.next()) {
@@ -175,13 +91,15 @@ public class AdminDAO {
   }
 
   public boolean deleteMember(String userId) {
+    String sql = "DELETE FROM member WHERE id = ?";
+
     boolean isDeleted = false;
     Connection conn = null;
     PreparedStatement pstmt = null;
 
     try {
       conn = getConnection();
-      pstmt = conn.prepareStatement(DELETE_MEMBER_BY_ID);
+      pstmt = conn.prepareStatement(sql);
       pstmt.setString(1, userId);
 
       int rows = pstmt.executeUpdate();
@@ -197,34 +115,8 @@ public class AdminDAO {
     return isDeleted;
   }
 
-  public String selectAdminPW() {
-    String adminPW = "";
-
-    Connection conn = null;
-    PreparedStatement pstmt = null;
-    ResultSet rs = null;
-
-    try {
-      conn = getConnection();
-      pstmt = conn.prepareStatement(SELECT_ADMIN_PW);
-      rs = pstmt.executeQuery();
-
-      while (rs.next()) {
-        adminPW = rs.getString(1);
-      }
-
-      return adminPW;
-
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
-    } finally {
-      closeResource(pstmt, conn, rs);
-    }
-  }
-
-  public List<StoreAndBossDTO> getStoreBusinessInfo(String previousBusinessNumber, int limit) {
-    String sql = "SELECT s.store_name, s.business_number, m.id, m.created_at FROM store_business_info s LEFT JOIN member m ON s.business_number = m.business_number where s.business_number < ? ORDER BY s.business_number DESC LIMIT ?";
-
+  public List<StoreAndBossDTO> selectStoreBusinessInfo() {
+    String sql = "SELECT s.store_name, s.business_number, m.id, m.created_at FROM store_business_info s LEFT JOIN member m ON s.business_number = m.business_number";
     List<StoreAndBossDTO> list = new ArrayList<>();
 
     Connection conn = null;
@@ -234,54 +126,9 @@ public class AdminDAO {
     try {
       conn = getConnection();
       pstmt = conn.prepareStatement(sql);
-      pstmt.setString(1, previousBusinessNumber);
-      pstmt.setInt(2, limit);
       rs = pstmt.executeQuery();
 
-      while (rs.next()) {
-        StoreAndBossDTO storeAndBoss = new StoreAndBossDTO();
-        storeAndBoss.setStoreName(rs.getString(1));
-        storeAndBoss.setBusinessNumber(rs.getString(2));
-        storeAndBoss.setId(rs.getString(3));
-        storeAndBoss.setCreatedAt(rs.getTimestamp(4));
-
-        list.add(storeAndBoss);
-      }
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
-    } finally {
-      closeResource(pstmt, conn, rs);
-    }
-
-    return list;
-  }
-
-  public List<StoreAndBossDTO> getStoreBusinessInfo(String previousBusinessNumber, int limit, String searchWord) {
-    String sql = "SELECT s.store_name, s.business_number, m.id, m.created_at " +
-        "FROM store_business_info s " +
-        "LEFT JOIN member m ON s.business_number = m.business_number " +
-        "WHERE s.business_number < ? " +
-        "AND (s.store_name LIKE CONCAT('%', ?, '%') OR m.id LIKE CONCAT('%', ?, '%')) " +
-        "LIMIT ?";
-
-    List<StoreAndBossDTO> list = new ArrayList<>();
-
-
-    Connection conn = null;
-    PreparedStatement pstmt = null;
-    ResultSet rs = null;
-
-    try {
-      conn = getConnection();
-      pstmt = conn.prepareStatement(sql);
-      int paramIndex = 1;
-      pstmt.setString(paramIndex++, previousBusinessNumber);
-      pstmt.setString(paramIndex++, searchWord);
-      pstmt.setString(paramIndex++, searchWord);
-      pstmt.setInt(paramIndex, limit);
-      rs = pstmt.executeQuery();
-
-      while (rs.next()) {
+      while(rs.next()) {
         StoreAndBossDTO storeAndBoss = new StoreAndBossDTO();
         storeAndBoss.setStoreName(rs.getString(1));
         storeAndBoss.setBusinessNumber(rs.getString(2));
@@ -300,13 +147,14 @@ public class AdminDAO {
   }
 
   public boolean deleteStore(String businessNumber) {
+    String sql = "DELETE FROM store_business_info WHERE business_number = ?";
     boolean isDeleted = false;
     Connection conn = null;
     PreparedStatement pstmt = null;
 
     try {
       conn = getConnection();
-      pstmt = conn.prepareStatement(DELETE_STORE_BY_BUSINESS_NUMBER);
+      pstmt = conn.prepareStatement(sql);
       pstmt.setString(1, businessNumber);
 
       int rows = pstmt.executeUpdate();
@@ -323,13 +171,15 @@ public class AdminDAO {
   }
 
   public boolean insertStore(StoreBusinessInfo storeBusinessInfo) {
+    String sql = "INSERT INTO store_business_info(business_number, store_name) VALUES(?, ?)";
+
     boolean isInserted = false;
     Connection conn = null;
     PreparedStatement pstmt = null;
 
     try {
       conn = getConnection();
-      pstmt = conn.prepareStatement(INSERT_STORE);
+      pstmt = conn.prepareStatement(sql);
       pstmt.setString(1, storeBusinessInfo.getBusinessNumber());
       pstmt.setString(2, storeBusinessInfo.getStoreName());
 
